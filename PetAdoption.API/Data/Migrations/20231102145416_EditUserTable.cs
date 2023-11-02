bc1e68f2c6735036408e2c024cb121bb32a0ce15
Konstantin Kirchev <konstantin.kirchev@sportingtech.com>
2023-11-02 17:27:58 +0200

    Add data migrations

---
+++ PetAdoption.API/Data/Migrations/20231102145416_EditUserTable.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PetAdoption.API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class EditUserTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Hash",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "Salt",
+                table: "Users",
+                newName: "Password");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Password",
+                table: "Users",
+                newName: "Salt");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Hash",
+                table: "Users",
+                type: "nvarchar(80)",
+                maxLength: 80,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
