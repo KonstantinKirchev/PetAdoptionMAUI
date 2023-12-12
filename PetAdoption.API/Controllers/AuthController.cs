b6762838fbcf19432e5bf7f3abfd650bfe1593fa
konstantinkirchev <konstantin1230@yahoo.com>
2023-12-12 14:42:02 +0200

    Add auth, pets and user controllers

---
+++ PetAdoption.API/Controllers/AuthController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PetAdoption.API.Services.Interfaces;
+using PetAdoption.Shared.Dtos;
+
+namespace PetAdoption.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        // api/auth/login
+        [HttpPost("login")] 
+        public async Task<ApiResponse<AuthResponseDto>> Login(LoginRequestDto dto) =>
+            await _authService.LoginAsync(dto);
+
+        // api/auth/register
+        [HttpPost("register")] 
+        public async Task<ApiResponse<AuthResponseDto>> Register(RegisterRequestDto dto) =>
+            await _authService.RegisterAsync(dto);
+    }
+}
+
---
+++ PetAdoption.API/Controllers/UserController.cs
@@ -0,0 +1,44 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PetAdoption.API.Services.Interfaces;
+using PetAdoption.Shared.Dtos;
+
+namespace PetAdoption.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserPetService _userPetService;
+
+        public UserController(IUserPetService userPetService)
+        {
+            _userPetService = userPetService;
+        }
+
+        private int UserId => Convert.ToInt32(User.Claims.First(c => c.Type == ClaimTypes.NameIdentifier).Value);
+
+        // GET: api/user/adoptions
+        [HttpGet("adoptions")]
+        public async Task<ApiResponse<PetListDto[]>> GetUserAdoptionsAsync() =>
+            await _userPetService.GetUserAdoptionsAsync(UserId);
+
+        // GET: api/user/favorites
+        [HttpGet("favorites")]
+        public async Task<ApiResponse<PetListDto[]>> GetUserFavoritesAsync() =>
+            await _userPetService.GetUserFavoritesAsync(UserId);
+
+        // POST: api/user/favorites/1
+        [HttpPost("favorites/{petId: int}")]
+        public async Task<ApiResponse> ToggleFavoritesAsync(int petId) =>
+            await _userPetService.ToggleFavoritesAsync(UserId, petId);
+
+        // POST: api/user/adopt/1
+        [HttpPost("adopt/{petId: int}")]
+        public async Task<ApiResponse> AdoptPetAsync(int petId) =>
+            await _userPetService.AdoptPetAsync(UserId, petId);
+    }
+}
+
