d7ba542a5f106733e1dc9cf462949235267a4da1
Konstantin Kirchev <konstantin.kirchev@sportingtech.com>
2023-12-15 17:36:14 +0200

    Add all the pages that we will need

---
+++ PetAdoption.Mobile/Pages/DetailsPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace PetAdoption.Mobile.Pages;
+
+public partial class Details : ContentPage
+{
+	public Details()
+	{
+		InitializeComponent();
+	}
+}
---
+++ PetAdoption.Mobile/Pages/HomePage.xaml.cs
@@ -0,0 +1,9 @@
+namespace PetAdoption.Mobile.Pages;
+
+public partial class Home : ContentPage
+{
+	public Home()
+	{
+		InitializeComponent();
+	}
+}
