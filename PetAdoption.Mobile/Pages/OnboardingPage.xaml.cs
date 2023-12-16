a0a5563c1a48288f9d7cd8ccb824513381447c8c
konstantinkirchev <konstantin1230@yahoo.com>
2023-12-16 14:06:10 +0200

    Set onboarding page to default preferences

--- PetAdoption.Mobile/Pages/OnboardingPage.xaml.cs
+++ PetAdoption.Mobile/Pages/OnboardingPage.xaml.cs
@@ -5,5 +5,7 @@ public partial class OnboardingPage : ContentPage
 	public OnboardingPage()
 	{
 		InitializeComponent();
+
+		Preferences.Default.Set(UIConstants.OnboardingShown, string.Empty);
 	}
 }
