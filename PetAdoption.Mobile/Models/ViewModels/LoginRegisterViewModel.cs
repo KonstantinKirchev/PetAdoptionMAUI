cead0c1e6c886f883b1379b8032eca225aebf15d
konstantinkirchev <konstantin1230@yahoo.com>
2024-01-09 14:43:51 +0200

    Add login/register model and view model

---
+++ PetAdoption.Mobile/Models/ViewModels/LoginRegisterViewModel.cs
@@ -0,0 +1,54 @@
+﻿using CommunityToolkit.Maui.Alerts;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using PetAdoption.Mobile.Pages;
+
+namespace PetAdoption.Mobile.Models.ViewModels
+{
+	[QueryProperty(nameof(IsFirstTime), nameof(IsFirstTime))]
+	public partial class LoginRegisterViewModel : ObservableObject
+	{
+		[ObservableProperty]
+		private bool _isRegistrationMode;
+
+		[ObservableProperty]
+		private LoginRegisterModel _model; 
+
+		[ObservableProperty]
+		private bool? _isFirstTime;
+
+		[ObservableProperty]
+		private bool _isBusy;
+
+		public void Initialize()
+		{
+			if(IsFirstTime.HasValue && IsFirstTime.Value)
+				IsRegistrationMode = true;
+		}
+
+		[RelayCommand]
+		private void ToggleMode() => IsRegistrationMode = !IsRegistrationMode;
+
+		[RelayCommand]
+		private async Task SkipForNow() =>
+			await Shell.Current.GoToAsync($"//{nameof(HomePage)}");
+
+		[RelayCommand]
+		private async Task Submit()
+		{
+			if (!Model.Validate(IsRegistrationMode))
+			{
+				await Toast.Make("All fields are required").Show();
+				return;
+			}
+
+			IsBusy = true;
+
+			//Make API call to login/register user
+			await Task.Delay(1000);
+			await SkipForNow();
+			IsBusy = false;
+		}
+	}  
+}
+
