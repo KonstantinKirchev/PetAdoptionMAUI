e614d48ea5948bea782920f1c831303b0edf7d20
konstantinkirchev <konstantin1230@yahoo.com>
2024-01-09 15:53:07 +0200

    Add query parameter isFirstTime user

--- PetAdoption.Mobile/Pages/OnboardingPage.xaml.cs
+++ PetAdoption.Mobile/Pages/OnboardingPage.xaml.cs
@@ -11,6 +11,6 @@ public partial class OnboardingPage : ContentPage
 
 	private async void Explore_Button_Clicked(object sender, EventArgs e)
 	{
-		await Shell.Current.GoToAsync($"//{nameof(LoginRegisterPage)}");
+		await Shell.Current.GoToAsync($"//{nameof(LoginRegisterPage)}?{nameof(LoginRegisterViewModel.IsFirstTime)}={true}");
 	}
 }
