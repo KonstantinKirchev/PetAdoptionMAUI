fbdc51730e174ed41094f74c317de4f88635e777
Konstantin Kirchev <konstantin1230@yahoo.com>
2024-12-09 15:30:20 +0200

    On boarding page is ready

--- PetAdoption.Client/Pages/OnboardingPage.xaml.cs
+++ PetAdoption.Client/Pages/OnboardingPage.xaml.cs
@@ -2,8 +2,19 @@ namespace PetAdoption.Client.Pages;
 
 public partial class OnboardingPage : ContentPage
 {
-	public OnboardingPage()
-	{
-		InitializeComponent();
-	}
+    public OnboardingPage()
+    {
+        InitializeComponent();
+
+        Preferences.Default.Set(UIConstants.OnboardingShown, string.Empty);
+    }
+
+    private async void Explore_Button_Clicked(object sender, EventArgs e)
+    {
+        var parameters = new Dictionary<string, object>
+        {
+            ["IsFirstTime"] = true
+        };
+        await Shell.Current.GoToAsync($"//{nameof(LoginRegisterPage)}", parameters);
+    }
 }
